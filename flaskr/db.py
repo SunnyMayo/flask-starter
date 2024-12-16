ec56bb03ad4c31e0017f551b0cbdf1c30df07bf7
Mason <150401281+SunnyMayo@users.noreply.github.com>
2024-12-16 14:12:46 +1100

    add db and register with application

--- flaskr/db.py
+++ flaskr/db.py
@@ -20,4 +20,23 @@ def close_db(e=None):
     if db is not None:
         db.close()
         
+def init_db():
+    db = get_db()
+
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+
+@click.command('init.db')
+def init_db_command():
+    """Clear existing table and create new"""
+    init_db()
+    click.echo('Initialised the database.')
+
+sqlite3.register_converter(
+    "timestamp", lambda v: datetime.fromisoformat(v.decode())
+)
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
 
